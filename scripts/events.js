faea5091cc545371afc0d0dbe26a506d44e9371e
Aeon Firebrand <lichking21st@windowslive.com>
2016-07-22 16:44:42 +1200

    Fixed login bug
    Accidentally checked whether function exists rather than whether
    function is true or false.

--- scripts/events.js
+++ scripts/events.js
@@ -1,7 +1,7 @@
 // Document Ready Event
 $(function() {
   // Check if already logged in
-  if (Login.isLoggedIn) {
+  if (Login.isLoggedIn()) {
     Login.toDashboard();
   }
   else {
@@ -15,6 +15,7 @@ $(function() {
     Login.toDashboard();
   });
 
+  // Attach event handler to sign out button
   $('#sign-out').click(function(event) {
     event.preventDefault();
     Login.signOut();
